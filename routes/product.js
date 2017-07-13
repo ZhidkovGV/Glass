1d5caae6671e9d821373aa7bacc98b03e8405ab1
Tsikhan Shcharbakou <tsikhan.shcharbakou@leverx.com>
2017-07-13 03:54:03 +0300

    product endpoint

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,7 @@ var Product = require('../models/product');
 
 router.get('/:cuid', function (req, res) {
     Product.findOne({_id : req.params.cuid},function (err, product) {
-        res.send(200,JSON.stringify(product || 'хуй'));
+        res.render('product', { product: product} );
     })
 });
 
